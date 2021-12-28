9fbbfe8d30f7fb606c469f3e9fd9a4c4abcb2411
Jan Skurski <jan.skurski@gmail.com>
2021-12-28 02:18:57 +0100

    feat: Add TTL option

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 import os
+from time import time
 from typing import Dict, Optional, Union
 
 from flask import Flask, render_template, request
@@ -16,7 +17,7 @@ app = Flask(__name__, static_folder=STATIC_DIR)
 
 
 @app.errorhandler(404)
-def page_not_found(e) -> ResponseReturnValue:
+def page_not_found(e=None) -> ResponseReturnValue:
     return "Invalid address!", 404
 
 
@@ -30,21 +31,29 @@ def index() -> ResponseReturnValue:
     return render_template("index.html")
 
 
+def _fresh_note(name: str) -> ResponseReturnValue:
+    context = {"name": name, "authorized": True}
+    return render_template("notes.html", context=context)
+
+
 @app.route("/<name>", methods=["GET"])
 def get_note(name):
     context = {"name": name}
     note_path = os.path.join(DATA_DIR, f"{name}.json")
     note_existed = os.path.isfile(note_path)
     if not note_existed:
-        context["authorized"] = True
-        return render_template("notes.html", context=context)
+        return _fresh_note(name)
     with open(note_path, "r") as inf:
         data = json.loads(inf.read())
+    if (ttl := data.get("ttl")) and ttl < time():
+        os.remove(note_path)
+        return _fresh_note(name)
     if data.get("password"):
         context["message"] = "Note is password-protected"
         return render_template("notes.html", context=context)
     context["authorized"] = True
     context["content"] = data["content"]
+    context["ttl"] = int(data["ttl"] - time()) if data["ttl"] is not None else ""
     return render_template("notes.html", context=context)
 
 
@@ -54,45 +63,67 @@ def post_note(name) -> ResponseReturnValue:
     note_path = os.path.join(DATA_DIR, f"{name}.json")
     note_existed = os.path.isfile(note_path)
     content = request.form.get("content")
+    get_ttl = request.form.get("ttl", "")
+    try:
+        ttl = int(get_ttl)
+    except ValueError:
+        ttl = None
+    else:
+        if ttl <= 0:
+            return _fresh_note(name)
+    ttl_timestamp = ttl + time() if ttl is not None else None
     password = ""
     if (request_password := request.form.get("password")):
         password = HASHING_ALGORITHM(request_password.encode()).hexdigest()
     if not note_existed:
-        return _post_new_note(content, context, note_path, password)
-    return _post_existing_note(content, context, note_path, password)
+        return _post_new_note(content, context, note_path, password, ttl_timestamp)
+    return _post_existing_note(content, context, note_path, password, ttl_timestamp)
 
 
 def _post_existing_note(
-    content: Optional[str], context: Dict[str, Union[str, bool, None]], note_path: str, password: str
+    content: Optional[str],
+    context: Dict[str, Union[str, bool, None]],
+    note_path: str,
+    password: str,
+    ttl: Optional[int],
 ) -> ResponseReturnValue:
     with open(note_path, "r") as inf:
         data = json.loads(inf.read())
+    if (data_ttl := data["ttl"]) is not None and data_ttl < time():
+        os.remove(note_path)
+        return _fresh_note(context["name"])
     if (previous_password := data["password"]) and (previous_password != password):
         return unauthorized()
     context["authorized"] = True
     if content is None:
         context["content"] = data["content"]
         return render_template("notes.html", context=context)
-    data = {"content": content, "password": password}
+    data = {"content": content, "password": password, "ttl": ttl}
     with open(note_path, "w") as ouf:
         ouf.write(json.dumps(data))
     context["message"] = "Note saved"
     context["content"] = content
+    context["ttl"] = context["ttl"] = int(ttl - time()) if ttl is not None else ""
     return render_template("notes.html", context=context)
 
 
 def _post_new_note(
-    content: Optional[str], context: Dict[str, Union[str, bool, None]], note_path: str, password: str
+    content: Optional[str],
+    context: Dict[str, Union[str, bool, None]],
+    note_path: str,
+    password: str,
+    ttl: Optional[int],
 ) -> ResponseReturnValue:
-    data = {"content": content, "password": password}
+    data = {"content": content, "password": password, "ttl": ttl}
     with open(note_path, "w") as ouf:
         ouf.write(json.dumps(data))
     context["message"] = "Note saved"
     context["authorized"] = True
     context["content"] = content
+    context["ttl"] = int(ttl - time()) if ttl is not None else ""
     return render_template("notes.html", context=context)
 
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host="0.0.0.0", debug=True, port=80)
+    app.run(host="0.0.0.0", debug=True, port=8001)
--- test/test_app.py
+++ test/test_app.py
@@ -1,5 +1,6 @@
 import shutil
 import tempfile
+import time
 from unittest.mock import patch
 
 from bs4 import BeautifulSoup
@@ -141,6 +142,51 @@ def test_wrong_password(client):
 
     # then
     assert rv.status_code == 403
+
+
+def test_with_ttl(client):
+    # given
+    rv = client.post("/my_note", data={"content": "abc", "ttl": "500"})
+
+    # when
+    rv = client.get("/my_note")
+
+    # then
+    assert rv.status_code == 200
+    soup = BeautifulSoup(rv.data)
+    form_element = soup.find("form")
+    retrieved_ttl = int(form_element.find("input", {"name": "ttl"}).get("value"))
+    assert 500 > retrieved_ttl > 490
+
+
+def test_ttl_reached(client):
+    # given
+    rv = client.post("/my_note", data={"content": "abc", "ttl": "1"})
+
+    # when
+    rv1 = client.get("/my_note")
+    time.sleep(1)
+    rv2 = client.get("/my_note")
+
+    # then
+    assert rv1.status_code == 200
+    assert rv2.status_code == 200
+    assert b"abc" in rv1.data
+    assert not b"abc" in rv2.data
+
+
+def test_ttl_minus(client):
+    # given
+    rv = client.post("/my_note", data={"content": "abc", "ttl": "-1"})
+
+    # when
+    rv = client.get("/my_note")
+
+    # then
+    soup = BeautifulSoup(rv.data)
+    form_element = soup.find("form")
+    assert form_element
+    assert form_element.find("textarea").text == ""
 
 
 def test_404(client):
